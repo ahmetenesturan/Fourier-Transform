0764a60cdbf5c34851581e281c2ba2212cc77c03
ahmetenesturan <ahmetenesturan@gmail.com>
2022-02-21 16:10:14 +0300

    minor changes

--- fourier.cpp
+++ fourier.cpp
@@ -5,52 +5,7 @@
 using namespace std;
 
 const double pi = atan(1) * 4;
-/*
-class ComplexNum
-{
-public:
-	double real;
-	double imag;
-	ComplexNum(double r, double i)
-	{
-		real = r;
-		imag = i;
-	}
-	ComplexNum(double r)
-	{
-		real = r;
-		imag = 0;
-	}
-	ComplexNum()
-	{
-		real = 0;
-		imag = 0;
-	}
-	ComplexNum& operator+(ComplexNum a)
-	{
-		ComplexNum r = ComplexNum((this->real + a.real), (this->imag + a.imag));
-		return r;
-	}
-	ComplexNum& operator*(ComplexNum a)
-	{
-		double real_part = (this->real * a.real) - (this->imag * a.imag);
-		double imag_part = (this->real * a.imag) + (this->imag * a.real);
-		ComplexNum r = ComplexNum(real_part, imag_part);
-		return r;
-	}
-	string print()
-	{
-		string s = to_string(real) + " + j * " + to_string(imag);
-		return s;
-	}
-};
 
-inline ComplexNum complexExp(double a)
-{
-	ComplexNum r = ComplexNum(cos(a), sin(a));
-	return r;
-}
-*/
 inline vector <ComplexNum> dft(vector <double> x, int size = 0)
 {
 	if (size == 0) size = x.size();
