a69a35e53e5454975eb6d83a01fd8952185835c3
ahmetenesturan <ahmetenesturan@gmail.com>
2022-02-24 23:00:13 +0300

    minor change

--- fourier.h
+++ fourier.h
@@ -1,8 +1,26 @@
 #pragma once
-#include "fourier.cpp"
+#include <cmath>
+#include <vector>
+#include "complex.h"
 
-extern const double pi;
+using namespace std;
 
-inline ComplexNum complexExp(double a);
+const double pi = atan(1) * 4;
 
-inline vector <ComplexNum> dft(vector <double> x, int size);
+inline vector <ComplexNum> dft(vector <double> x, int size = 0)
+{
+	if (size == 0) size = x.size();
+	vector <ComplexNum> out;
+	ComplexNum c;
+	for (int k = 0; k < size; k++)
+	{
+		c.real = 0;
+		c.imag = 0;
+		for (int n = 0; n < size; n++)
+		{
+			c = c + complexExp(-2 * pi * n * k / size) * x[n];
+		}
+		out.push_back(c);
+	}
+	return out;
+}
